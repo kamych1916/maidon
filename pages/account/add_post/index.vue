7868200ba3c324858e34e97eb829053b97fb5b11
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2021-04-03 20:41:41 +0300

    change: structure of template inputs offer

--- pages/account/add_post/index.vue
+++ pages/account/add_post/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pb-100">
+  <div class="pb-100 pt-50">
     <OfferTypes @checkOfferTypes="checkOfferTypes"></OfferTypes>
     <div ref="inputs">
       <div class="card-wrap mt-50">
@@ -20,78 +20,73 @@
           <OfferMap @onClickMap="onClickMap" :coord="coords"></OfferMap>
         </client-only>
       </div>
-      <div class="card-wrap mt-50">
-        <h4>Об объекте</h4>
-        <div class="row">
-          <div class="col-md-6 d-flex align-items-center my-20">
-            <div class="col-md-3 px-0">Общая площадь</div>
-            <el-input class="col-md-6 px-0" type="text" />
-          </div>
-          <div class="col-md-6 d-flex align-items-center my-20">
-            <div class="col-md-4 px-0">Количество комнат</div>
-            <el-input class="col-md-6 px-0" type="text" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 d-flex align-items-center my-20">
-            <div class="col-md-3 px-0">Этаж</div>
-            <div class="col-md-6 px-0">
-              <el-input-number
-                class="w-100"
-                v-model="offerData.floor"
-                :min="1"
-                :max="10"
-              ></el-input-number>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card-wrap mt-50">
+            <h4>Об объекте</h4>
+            <div class="row mx-0 align-items-center">
+              <div class="col-md-6 my-10">Общая площадь</div>
+              <el-input class="col-md-6 my-10" type="number" />
+              <div class="col-md-6 my-10">Количество комнат</div>
+              <el-input class="col-md-6 my-10" type="number" />
+              <div class="col-md-6 my-10 ">Этаж</div>
+              <div class="col-md-6 my-10">
+                <el-input-number
+                  v-model="offerData.floor"
+                  class="w-100"
+                  type="number"
+                  :min="1"
+                  :max="10"
+                ></el-input-number>
+              </div>
+              <div class="col-md-6 my-10 ">Этажей в доме</div>
+              <div class="col-md-6 my-10">
+                <el-input-number
+                  v-model="offerData.floorsHous"
+                  class="w-100"
+                  type="number"
+                  :min="1"
+                  :max="10"
+                ></el-input-number>
+              </div>
+              <div class="col-md-6 my-10 ">Тип застройки</div>
+              <div class="col-md-6 my-10">
+                <el-select
+                  placeholder=""
+                  class="w-100"
+                  v-model="offerData.building_type"
+                >
+                  <el-option
+                    v-for="item in building_type"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  >
+                  </el-option>
+                </el-select>
+              </div>
+              <div class="col-md-6 my-10 ">Ремонт</div>
+              <div class="col-md-6 my-10">
+                <el-select
+                  class="w-100"
+                  placeholder=""
+                  v-model="offerData.building_renovation"
+                >
+                  <el-option
+                    v-for="item in building_renovation"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  >
+                  </el-option>
+                </el-select>
+              </div>
             </div>
-          </div>
-          <div class="col-md-6 d-flex align-items-center my-20">
-            <div class="col-md-4 px-0">Этажей в доме</div>
-            <div class="col-md-6 px-0">
-              <el-input-number
-                v-model="offerData.floorsHous"
-                class="w-100"
-                type="number"
-                :min="1"
-                :max="10"
-              ></el-input-number>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 d-flex align-items-center my-20">
-            <div class="col-md-3 px-0">Тип застройки</div>
-            <el-select
-              placeholder=""
-              v-model="offerData.building_type"
-              class="col-md-6 px-0"
-            >
-              <el-option
-                v-for="item in building_type"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              >
-              </el-option>
-            </el-select>
-          </div>
-          <div class="col-md-6 d-flex align-items-center my-20">
-            <div class="col-md-4 px-0">Ремонт</div>
-            <el-select
-              placeholder=""
-              v-model="offerData.building_renovation"
-              class="col-md-6 px-0"
-            >
-              <el-option
-                v-for="item in building_renovation"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              >
-              </el-option>
-            </el-select>
           </div>
         </div>
       </div>
+
+      <div class="col-md-6"></div>
       <div class="row">
         <div class="col-md-6">
           <div class="card-wrap mt-50"></div>
