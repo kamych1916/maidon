487275c5abae19e6f676f2229068367cd77c55a5
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2021-05-04 21:45:45 +0300

    add: new template and logic for list chats pages

--- pages/account/chat/index.vue
+++ pages/account/chat/index.vue
@@ -5,47 +5,71 @@
       <div class="row" v-if="listChats.length > 0">
         <div class="col-xl-4" v-for="(item, idx) in listChats" :key="idx">
           <div
-            class="card-wrap d-flex justify-content-between"
+            class="card-wrap d-flex justify-content-between cursor"
+            style="position: relative"
             @click="$router.push(currentPath + '/' + item.id)"
-            style="cursor: pointer"
           >
-            <div class="d-flex">
-              <div class="avatar">
-                <el-image
-                  draggable="false"
-                  class="w-100 h-100 border-rad-5"
-                  :src="item.image"
-                  fit="cover"
-                ></el-image>
-              </div>
-              <div class="ml-10 d-flex align-items-center">
+            <div class="row d-flex">
+              <div class="col d-flex" style="display: -webkit-inline-box">
                 <div>
-                  {{ item.user_name }}<br />
-                  <span class="text-grey fs-12">{{ item.title }}</span>
-                </div>
-              </div>
-            </div>
-            <div class="d-flex align-items-center">
-              <el-popover trigger="hover" width="183" placement="top">
-                <div>
-                  <p>Удалить данный чат?</p>
-                  <div style="text-align: right; margin: 0">
-                    <el-button
-                      @click="delete_chat(item.id)"
-                      type="danger"
-                      size="mini"
-                    >
-                      Да
-                    </el-button>
+                  <div class="avatar">
+                    <el-image
+                      draggable="false"
+                      class="w-100 h-100 border-rad-5"
+                      :src="item.image"
+                      fit="cover"
+                    ></el-image>
                   </div>
                 </div>
-                <button
-                  slot="reference"
-                  class="el-button el-button--primary is-round fs-14 py-10 px-20 mx-5 my-5"
-                >
-                  <i class="bi bi-trash"></i>
-                </button>
-              </el-popover>
+
+                <div class="ml-10 ">
+                  <div>
+                    <span style="word-break: break-all;">
+                      <!-- {{ item.user_name }} -->
+                      {{ item.user_name.split(" ")[1] }}<br />
+                      {{ item.user_name.split(" ")[0] }}
+                      <el-tooltip
+                        v-if="!item.unread"
+                        class="item"
+                        effect="dark"
+                        placement="top-start"
+                      >
+                        <div slot="content">
+                          У вас имеются непрочитанные сообщения
+                        </div>
+                        <span class="text-blue ">
+                          <i class="bi bi-chat-dots-fill fs-12 ml-8"></i>
+                        </span>
+                      </el-tooltip>
+                    </span>
+                    <br />
+                    <span class="text-grey fs-12">{{ item.title }}</span
+                    ><br />
+                  </div>
+                </div>
+              </div>
+              <div style="position: absolute; top: 40px;right: 20px">
+                <el-popover trigger="hover" width="183" placement="top">
+                  <div>
+                    <p>Удалить данный чат?</p>
+                    <div style="text-align: right; margin: 0">
+                      <el-button
+                        @click="delete_chat(item.id)"
+                        type="danger"
+                        size="mini"
+                      >
+                        Да
+                      </el-button>
+                    </div>
+                  </div>
+                  <button
+                    slot="reference"
+                    class="el-button el-button--primary is-round fs-14 py-10 px-20 mx-5 my-5"
+                  >
+                    <i class="bi bi-trash"></i>
+                  </button>
+                </el-popover>
+              </div>
             </div>
           </div>
         </div>
