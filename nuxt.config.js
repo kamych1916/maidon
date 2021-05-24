1e98d0303504cf561536a0f6e1fa8a92af4d79b6
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2021-05-24 19:52:11 +0500

    add: setting universal mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ export default {
   //   port: 8000 // default: 3000
   // },
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  mode: "universal",
   ssr: true,
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
